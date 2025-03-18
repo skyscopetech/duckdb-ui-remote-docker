471690bd003b0b14f5e81fb6709824b971db679d
Vincent Colmenar <vcolmenar@gmail.com>
2025-03-17 20:39:08 -0400

    Add Logging (#6)

--- startup.sh
+++ startup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Run DuckDB UI in screen
-screen -dmS duckdb_ui /var/lib/haproxy/.duckdb/cli/latest/duckdb -ui
+screen -L -Logfile ./duckdb_ui.log -dmS duckdb_ui /var/lib/haproxy/.duckdb/cli/latest/duckdb -ui
 
 # Run HAProxy
-haproxy -f /usr/local/etc/haproxy/haproxy.cfg
+screen -L -Logfile ./haproxy.log -dmS haproxy_app haproxy -f /usr/local/etc/haproxy/haproxy.cfg
+
+# Tail both logs
+sleep 5
+tail -f ./duckdb_ui.log ./haproxy.log
